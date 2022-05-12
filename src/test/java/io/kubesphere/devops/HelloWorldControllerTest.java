c5d10f30ca8e95755a19dd4b27cac134fc6657d3
우주희 <56661116+woojuhee@users.noreply.github.com>
2022-05-12 16:29:01 +0900

    Update HelloWorldControllerTest.java

--- src/test/java/io/kubesphere/devops/HelloWorldControllerTest.java
+++ src/test/java/io/kubesphere/devops/HelloWorldControllerTest.java
@@ -8,6 +8,6 @@ public class HelloWorldControllerTest {
 
     @Test
     public void testSayHello() {
-        assertEquals("IBK POC", new HelloWorldController().sayHello());
+        assertEquals("IBK 시연환경입니다", new HelloWorldController().sayHello());
     }
 }
